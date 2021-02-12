c3490418cfa9c10ef539c317fc745d626a0845bb
InyaevaSvetlana <58864697+InyaevaSvetlana@users.noreply.github.com>
2021-02-12 10:38:01 +0300

    .

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const container = document.querySelector('#app');
+
+const content = <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi libero omnis inventore vel eaque repudiandae, voluptatum error provident iure cupiditate esse dicta dignissimos? Alias deleniti maiores rerum magnam veritatis iste </h2>;
+ReactDom.render(content, container);
